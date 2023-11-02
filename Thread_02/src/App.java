7bee41e3c2d22e0cbaf042173cd040cf775e86dc
Gianni Nacu <gianninacu0@gmail.com>
2023-11-02 11:45:17 +0000

    Thread_02

--- Thread_02/src/App.java
+++ Thread_02/src/App.java
@@ -1,5 +1,27 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        //Da questo punto partirà l'esecuzione del thread principale
+        int i = 1;
+
+        /*
+        > Recupero il thread principale in questi punto del programma
+        Thread t = Thread.currentThread();
+        t.setName("Thread Principale");
+        System.out.println(t.getName());
+        */
+        
+        Thread.currentThread().setName("Thread Principale");
+
+        System.out.println(Thread.currentThread().getName());
+        
+        //Creo un nuovo thread e gli assegno delle istruzioni da eseguire
+        Thread ts = new Thread(/* Passo un oggetto di una classe al cui interno è implementato il metodo RUN */);
+        
+
+
+
+
+
     }
 }
---
+++ Thread_02/src/ThreadSecondario.java
@@ -0,0 +1,7 @@
+public class ThreadSecondario {
+    
+    public void run(){
+        //inserisco qui le istruzioni che voglio far eseguire al nuovo Thread
+        
+    }
+}
