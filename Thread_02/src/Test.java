695f2f2c485af1c4a4f0a2e909f7f53d30425e54
Gianni Nacu <gianninacu0@gmail.com>
2023-11-02 12:33:28 +0000

    2

--- Thread_02/src/Test.java
+++ Thread_02/src/Test.java
@@ -13,19 +13,27 @@ public class Test {
         
         Thread.currentThread().setName("Thread Principale");
 
-        System.out.println(Thread.currentThread().getName());
-        
         //Creo un nuovo thread e gli assegno delle istruzioni da eseguire
         //Thread ts = new Thread(/* Passo un oggetto di una classe al cui interno è implementato il metodo RUN */);
 
         //ThreadSecondario t2 = new ThreadSecondario();
         //Thread ts = new Thread(t2);
 
-        Thread ts = new Thread(new ThreadSecondario()); 
+        Thread ts = new Thread(new ThreadSecondario());
+        //mando in esecuzione il thread secondario
+        ts.start();
 
+        for (int j = 0; j < 10; j++) {
+            System.out.println("Numero[" + j + "]\n");
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                System.out.println("ATTENZIONE!! Qualcuno mi ha svegliato anticipatamente!");
+                
+            }
+        }
 
-
-
+        System.out.println(Thread.currentThread().getName() + "Termina la sua esecuzuione\n");
 
     }
 }
--- Thread_02/src/ThreadSecondario.java
+++ Thread_02/src/ThreadSecondario.java
@@ -1,8 +1,24 @@
 public class ThreadSecondario implements Runnable{
     
+    
+
     public void run(){
         //inserisco qui le istruzioni che voglio far eseguire al nuovo Thread
+        Thread.currentThread().setName("Thread Secondario");
 
+        System.out.println("\t\t" + Thread.currentThread() + " inizia la sua esecuzione\n");
+
+        for (int i = 0; i < 30; i++) {
+            System.out.println("\t\tNumero[" + i + "]\n");
+            try {
+                Thread.sleep(300); // Ha effetto sul thread in esecuzione
+                //oppure → Thread.currentThread().sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("\t\tATTENZIONE!! Qualcuno ha svegliato il thread anticipatamente");
+            }
+        }
+
+        System.out.println("\t\t" + Thread.currentThread() + "finisce la sua esecuzione\n");
 
     }
 }
